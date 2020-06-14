992ea7e0cb666434a90d0d89de66a255758cd901
NaeemMeman <naeemmeman@gmail.com>
2020-06-13 23:10:02 -0400

    Updates

---
+++ precache-manifest.fa6f8e3fd442f16effb3d8ce3d9150d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a98387c2a7025d508cb0de291766506",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "47680d522f92e005ab13",
+    "url": "/react-tutorial/static/css/main.5a966e1e.chunk.css"
+  },
+  {
+    "revision": "118b680d6cfad604c017",
+    "url": "/react-tutorial/static/js/2.a0eb12dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.a0eb12dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47680d522f92e005ab13",
+    "url": "/react-tutorial/static/js/main.11d3663b.chunk.js"
+  },
+  {
+    "revision": "b6f2badd4a096922496c",
+    "url": "/react-tutorial/static/js/runtime-main.ee9240c0.js"
+  }
+]);
